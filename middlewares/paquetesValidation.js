952992bec3ef75febd593de68dac0f8b4ea8f5c3
Ashmesa17 <jaramillomesaashlley@gmail.com>
2025-03-06 07:09:51 -0500

    Modificacion en middleware paquetes

--- middlewares/paquetesValidation.js
+++ middlewares/paquetesValidation.js
@@ -3,14 +3,14 @@ const { body, validationResult } = require('express-validator')
 const paqueteBaseValidation = [
     body('nombre')
         .isString().withMessage('El nombre del paquete debe ser un texto')
-        .isLength({ min: 1 }).withMessage('El nombre del paquete debe tener al menos 1 carácter'),
+        .isLength({ min: 3 }).withMessage('El nombre del paquete debe tener al menos 3 caracteres'),
 
     body('valor')
         .isInt({ min: 0 }).withMessage('El valor del paquete debe ser un número entero mayor o igual a 0'),
 
     body('descripcion')
         .isString().withMessage('La descripción del paquete debe ser un texto')
-        .isLength({ min: 1 }).withMessage('La descripción del paquete debe tener al menos 1 carácter')
+        .isLength({ min: 3 }).withMessage('La descripción del paquete debe tener al menos 3 caracteres')
        
 ];
 const validate = (req, res, next) => {

7e523c20753ef9bcdb4b4a094aa4148566d1d7ec
Ximena <roblemariacastaneda@gmail.com>
2025-03-09 17:47:33 -0500

    Servicios Middleware, Controller, Routes, Models

--- routes/servicioRoutes.js
+++ routes/servicioRoutes.js
@@ -10,14 +10,20 @@ const {
     deleteServicio
 } = require('../controllers/servicioController');
 
+const {
+    postServicioValidation,
+    updateServicioValidation,
+    deleteServicioValidation,
+    getServicioByIdValidation,
+    validate
+} = require('../middlewares/servicioMiddleware');
+
 router.get('/', getServicios);
-router.get('/:id', getServicioById);
-router.post('/', createServicio);
-router.put('/:id', updateServicio);
-router.delete('/:id', deleteServicio);
+router.get('/:id', getServicioByIdValidation, validate, getServicioById);
+router.post('/', postServicioValidation, validate, createServicio);
+router.put('/:id', updateServicioValidation, validate, updateServicio);
+router.delete('/:id', deleteServicioValidation, validate, deleteServicio);
 
 router.use(errorMiddleware);
 
 module.exports = router;
-
-router.use(errorMiddleware);

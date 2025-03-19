aa3b3f96fca851dc77c8dfa24ff0271b17c1dd6a
K3v1nD3V <kctDEV494@gmail.com>
2025-03-19 16:49:18 -0500

    Completando validaciones de paquetes y privilegios

--- middlewares/paquetesValidation.js
+++ middlewares/paquetesValidation.js
@@ -1,14 +1,11 @@
 const { check } = require('express-validator')
 
 const validatePaquete = [
-    check('nombre')
-        .isString().withMessage('El nombre del paquete debe ser un texto')
-        .isLength({ min: 3 }).withMessage('El nombre del paquete debe tener al menos 3 caracteres'),
-    check('valor')
-        .isInt({ min: 0 }).withMessage('El valor del paquete debe ser un número entero mayor o igual a 0'),
-    check('descripcion')
-        .isString().withMessage('La descripción del paquete debe ser un texto')
-        .isLength({ min: 3 }).withMessage('La descripción del paquete debe tener al menos 3 caracteres')
+    check('nombre').isString().withMessage('El nombre del paquete debe ser un texto'),
+    check('nombre').isLength({ min: 3 }).withMessage('El nombre del paquete debe tener al menos 3 caracteres'),
+    check('valor').isInt({ min: 0 }).withMessage('El valor del paquete debe ser un número entero mayor o igual a 0'),
+    check('descripcion').isString().withMessage('La descripción del paquete debe ser un texto'),
+    check('descripcion').isLength({ min: 3 }).withMessage('La descripción del paquete debe tener al menos 3 caracteres')
        
 ];
 module.exports = validatePaquete
--- middlewares/privilegioValidation.js
+++ middlewares/privilegioValidation.js
@@ -1,9 +1,9 @@
 const { check } = require('express-validator');
 
 const validatePrivilegio = [
+    check('descripcion').notEmpty().withMessage('La descripción es requerida'),
     check('descripcion').isLength({ min: 5 }).withMessage('La descripción debe tener al menos 5 caracteres'),
     check('estado').isBoolean().withMessage('El estado debe ser un booleano')
 ];
-
 
 module.exports = validatePrivilegio;

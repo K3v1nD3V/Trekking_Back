3d4a3c04943cf4a88cd90a2900da34486d6163e9
K3v1nD3V <kctDEV494@gmail.com>
2025-03-31 09:36:03 -0500

    Añado el manejo de error en authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,28 +1,25 @@
 const jwt = require('jsonwebtoken');
-const Usuario = require('../models/usuario');
 
-const authMiddleware = (requiredPermissions = []) => {
-    return async (req, res, next) => {
-        const token = req.header('Authorization').replace('Bearer ', '');
-        if (!token) {
-            return res.status(401).json({ message: 'Acceso denegado. No se proporcionó token.' });
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    
+    if (!authHeader) {
+        return res.status(401).json({ error: 'No se proporcionó el token de autorización' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token de autorización no válido' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token de autorización no válido' });
         }
-        
-        try {
-            const verified = jwt.verify(token, process.env.JWT_SECRET);
-            const user = await Usuario.findById(verified.id).populate('rol');
-            req.user = user;
-
-            // Verificar permisos si se requieren
-            if (requiredPermissions.length > 0 && !requiredPermissions.includes(req.user.rol.nombre)) {
-                return res.status(403).json({ message: 'No tienes permisos para acceder a esta ruta.' });
-            }
-            next();
-        } catch (error) {
-            res.status(400).json({ message: 'Token no válido.' });
-        }
-    };
-
+        req.user = user;
+        next();
+    });
 };
 
 module.exports = authMiddleware;

857852b67fcdef963d378bcabaececf1c12395ad
kevin correa <kacete494@gmail.com>
2025-04-01 06:48:29 -0500

    re orden

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,25 +1,25 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    
-    if (!authHeader) {
-        return res.status(401).json({ error: 'No se proporcionó el token de autorización' });
-    }
-
-    const token = authHeader.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).json({ error: 'Token de autorización no válido' });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Token de autorización no válido' });
+const authMiddleware = (requiredPermissions = []) => {
+    return (req, res, next) => {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        if (!token) {
+            return res.status(401).json({ message: 'Acceso denegado. No se proporcionó token.' });
         }
-        req.user = user;
-        next();
-    });
+        
+        try {
+            const verified = jwt.verify(token, process.env.JWT_SECRET); // Usar el secreto desde .env
+            req.user = verified;
+            
+            // Verificar permisos si se requieren
+            if (requiredPermissions.length > 0 && !requiredPermissions.includes(req.user.role)) {
+                return res.status(403).json({ message: 'No tienes permisos para acceder a esta ruta.' });
+            }
+            next()
+        } catch (error) {
+            res.status(400).json({ message: 'Token no válido.' });
+        }
+    };
 };
 
 module.exports = authMiddleware;

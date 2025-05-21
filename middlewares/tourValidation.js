3ffc1946c9e898d22a95ac106b883179b4ea61e5
K3v1nD3V <kctDEV494@gmail.com>
2025-05-21 15:42:43 -0500

    se añade la validacion de fechas en tourValidation, la fecha del tour no puede ser menos que la fecha actual y la fecha limite no puede ser menor a la fecha del tour

--- middlewares/tourValidation.js
+++ middlewares/tourValidation.js
@@ -1,10 +1,16 @@
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const validateTour = [
     check('fechaHora')
         .isISO8601().withMessage('La fecha y hora deben tener un formato válido')
         .toDate()
-        .notEmpty().withMessage('La fecha y hora son requeridas'),
+        .notEmpty().withMessage('La fecha y hora son requeridas')
+        .custom((value) => {
+            if (new Date(value) < new Date()) {
+                throw new Error('La fecha y hora del tour no pueden ser menores a la fecha actual');
+            }
+            return true;
+        }),
     check('id_paquete')
         .isMongoId().withMessage('El ID del paquete no es válido')
         .notEmpty().withMessage('El ID del paquete es requerido'),
@@ -14,7 +20,13 @@ const validateTour = [
     check('fecha_limite_inscripcion')
         .isISO8601().withMessage('La fecha límite de inscripción debe tener un formato válido')
         .toDate()
-        .notEmpty().withMessage('La fecha límite de inscripción es requerida')
+        .notEmpty().withMessage('La fecha límite de inscripción es requerida'),
+    body().custom((value, { req }) => {
+        if (new Date(req.body.fecha_limite_inscripcion) >= new Date(req.body.fechaHora)) {
+            return true;
+        }
+        throw new Error('La fecha límite de inscripción no puede ser menor que la fecha y hora del tour');
+    })
 ];
 
 module.exports = validateTour;

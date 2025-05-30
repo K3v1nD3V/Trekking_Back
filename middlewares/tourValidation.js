45838c510a56ffa924a6c8c58c16ef2f646dd138
K3v1nD3V <kctDEV494@gmail.com>
2025-05-30 10:31:25 -0500

    cambio en las validaciones de tours

--- middlewares/tourValidation.js
+++ middlewares/tourValidation.js
@@ -20,13 +20,21 @@ const validateTour = [
     check('fecha_limite_inscripcion')
         .isISO8601().withMessage('La fecha límite de inscripción debe tener un formato válido')
         .toDate()
-        .notEmpty().withMessage('La fecha límite de inscripción es requerida'),
-    body().custom((value, { req }) => {
-        if (new Date(req.body.fecha_limite_inscripcion) >= new Date(req.body.fechaHora)) {
+        .notEmpty().withMessage('La fecha límite de inscripción es requerida')
+        .custom((value, { req }) => {
+            const fechaHora = new Date(req.body.fechaHora);
+            const fechaLimiteInscripcion = new Date(value);
+
+            if (fechaLimiteInscripcion >= fechaHora) {
+                throw new Error('La fecha límite de inscripción debe ser menor que la fecha y hora del tour');
+            }
+
+            if (fechaLimiteInscripcion < new Date()) {
+                throw new Error('La fecha límite de inscripción no puede ser menor a la fecha actual');
+            }
+
             return true;
-        }
-        throw new Error('La fecha límite de inscripción no puede ser menor que la fecha y hora del tour');
-    })
+        })
 ];
 
 module.exports = validateTour;

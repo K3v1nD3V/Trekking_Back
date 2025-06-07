c9661399298b13d2db370a08cbdba8a67d6112b1
K3v1nD3V <kctDEV494@gmail.com>
2025-06-07 08:19:29 -0500

    cambios minimos en las funciones de validacion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -200,7 +200,7 @@ const cambiarContraseña = async (req, res) => {
 const enviarCorreoVerificacion = async (usuario) => {
     
     const token = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '24h' }); // token expira en 24 horas cambiarlo, cambiarlo a 15 minutos
-    const link = `${process.env.FRONTEND_URL}/verificar/${token}`; //link que le llega al usuario
+    const link = `${process.env.FRONTEND_URL}/login?${token}`; //link que le llega al usuario
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -245,7 +245,7 @@ const verificarCorreo = async (req, res) => {
         }
 
         console.log("Usuario actualizado:", usuarioActualizado);
-
+        res.json({ msg: 'Cuenta verificada correctamente' });
         // res.redirect(`${process.env.FRONTEND_URL}/login`); // 👈 Redirige después de actualizar la BD
     } catch (error) {
         console.error("Error en verificación:", error);
